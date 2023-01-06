1cec1a9ca4e07bdc611fb07a92146c264b689b92
Ade Nur Faizal <adenufaizal31@gmail.com>
2023-01-06 18:18:24 +0700

    update

--- models/topping.go
+++ models/topping.go
@@ -5,7 +5,7 @@ import "time"
 type Topping struct {
 	ID          int    `json:"id"`
 	Nametopping string `json:"nametopping" form:"nametopping" gorm:"type: varchar(255)"`
-	Price       int    `json:"price" form:"price" gorm:"type: varchar(255)"`
+	Price       int    `json:"price" form:"price" gorm:"type: int"`
 	Image       string `json:"image" form:"image" gorm:"type: varchar(255)"`
 	//Qty      int          `json:"qty" form:"qty"`
 	CreateAt time.Time `json:"-"`
